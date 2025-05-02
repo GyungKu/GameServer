3bb3d0fbadb1e676e1d032889babdaf915a02d8c
gk <iba3374@naver.com>
2025-05-02 15:30:39 +0900

    first commit

---
+++ server/Program.cs
@@ -0,0 +1,18 @@
+﻿using MessagePack;
+using server.Network;
+
+namespace server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("=== 게임 서버 시작 ===");
+
+            TcpServer server = new TcpServer();
+            await server.StartAsync(); // 원하는 포트
+
+            Console.WriteLine("=== 서버 종료 ===");
+        }
+    }
+}
