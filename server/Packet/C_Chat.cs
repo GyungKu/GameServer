1f7d80dbbf10a308330475c22b1e29bb9cd5aca9
gk <iba3374@naver.com>
2025-05-02 15:34:17 +0900

    feat: 테스트용 채팅 추가

---
+++ server/Packet/C_Chat.cs
@@ -0,0 +1,15 @@
+﻿using MessagePack;
+
+namespace server.Packet;
+
+[MessagePackObject]
+public class C_Chat : PacketBase
+{
+    public override PacketType Type => PacketType.C_Chat;
+    
+    [Key(1)]
+    public int userId { get; set; }
+    
+    [Key(2)]
+    public string chat { get; set; }
+}
